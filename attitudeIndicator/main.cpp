948fd99e0639adb39b651b488df279d495f7ebb3
Sebastian <sebastianblari@gmail.com>
2019-05-28 22:46:17 -0600

    Update main.cpp

--- attitudeIndicator/main.cpp
+++ attitudeIndicator/main.cpp
@@ -6,7 +6,6 @@
 #include "FillScreen.hpp"
 #include "GetBorder.hpp"
 #include "GetDifference.hpp"
-#include "FillDifference.hpp"
 
 //----------------------------------------------------------------
 uint8_t Task::m_u8NextTaskID = 0;   //init task ID
@@ -19,19 +18,18 @@ int main(void)
 
     /** instantiate new Tasks **/
     GetBorder BorderLine;
-    DrawMark WhiteMark;
     FillScreen Fill;
     GetDifference GD;
-	FillDifference FillDiff;
 
 	/** Mailbox instance **/
     Mailbox g_MailHandler;
 
 
 	/** attach the Tasks to the Scheduler **/
-    g_MainScheduler.attach(&BorderLine, 100);
-    g_MainScheduler.attach(&WhiteMark, 101);
+    g_MainScheduler.attach(&BorderLine, 101);
     g_MainScheduler.attach(&Fill, 102);
+
+
 
 	/** run the Setup for the scheduler and all tasks **/
     g_MainScheduler.setup(&g_MailHandler);
@@ -59,7 +57,6 @@ int main(void)
     }
 
     g_MainScheduler.remove(&Fill);
-    g_MainScheduler.remove(&WhiteMark);
     g_MainScheduler.attach(&GD, 102);
     g_MainScheduler.setup(&g_MailHandler);
 
@@ -75,34 +72,4 @@ int main(void)
         }
     }
 
-    /*while(1)
-    {
-        //MAP_PCM_gotoLPM0(); //do not know what it does
-
-        __wfe(); // Wait for Event
-
-        if(g_SystemTicks != g_MainScheduler.m_u64Ticks)
-        {
-            //only execute the tasks if one tick has passed.
-            g_MainScheduler.m_u64Ticks = g_SystemTicks;
-            g_MainScheduler.run();
-            done = true;
-        }
-        g_MainScheduler.remove(&Fill);
-            //g_MainScheduler.remove(&WhiteMark);
-            g_MainScheduler.attach(&GD, 102);
-            g_MainScheduler.setup(&g_MailHandler);
-            while(done){
-                //MAP_PCM_gotoLPM0(); //do not know what it does
-
-                __wfe(); // Wait for Event
-
-                if(g_SystemTicks != g_MainScheduler.m_u64Ticks)
-                {
-                    //only execute the tasks if one tick has passed.
-                    g_MainScheduler.m_u64Ticks = g_SystemTicks;
-                    g_MainScheduler.run();
-                }
-            }
-    }*/
 }
